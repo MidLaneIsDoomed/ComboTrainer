0f529ed00062ad0c1f3aadb1df46d97fea5cf068
MidLaneIsDoomed <leo.ahlstrom08@gmail.com>
2026-03-27 13:30:08 +0100

    java

--- JS/logic.js
+++ JS/logic.js
@@ -1,5 +1,80 @@
-const keyPressed = document.getElementById("pressedKey");
+const airblade = ["e", "q", "r" ]
+let position = 0;
 
-keyPressed.addEventListener("keydown",function(event) {
-    document.getElementById("output").innerHTML = "you pressed: " + event.key
-});
+const pressedKey = document.getElementById("pressedKey-container");
+
+const actions = [
+    () => document.getElementById("Eoutput").style.backgroundColor = "#32CD32",
+    () => document.getElementById("Qoutput").style.backgroundColor = "#32CD32",
+    () => document.getElementById("Routput").style.backgroundColor = "32CD32"
+];
+
+let points = 0;
+    
+function resetStats() {
+    document.getElementById("points").innerHTML = ""
+}
+
+document.addEventListener("keydown", (event) => {
+    if (event.key === airblade[position]) {
+
+        actions[position]();
+        position++;
+        
+
+      if (position === airblade.length) {
+        console.log("airblade completed!");
+        position = 0;
+
+        document.getElementById("end-message").innerHTML = "Combo Completed"
+        document.getElementById("restart").innerHTML = "Press SPACE to restart"
+
+        document.getElementById("points").innerHTML = points++;
+
+        document.querySelectorAll(".outputs").forEach(el => { 
+            el.style.backgroundColor = "#32CD32";
+        });
+
+      }
+  
+    } else {
+      position = 0;
+      
+        document.querySelectorAll(".outputs").forEach(el => {
+            el.style.backgroundColor = "#ff0000";
+        });
+    }
+
+    if(event.key === " ") {
+
+        document.querySelectorAll(".outputs").forEach(el => {
+            el.style.backgroundColor = "#ff0000";
+
+            document.getElementById("end-message").innerHTML = ""
+        document.getElementById("restart").innerHTML = ""
+        });
+    }
+  });
+
+
+  
+
+
+
+
+
+
+// keyPressed.addEventListener("keydown",function(event) {
+//     if(event.key === "e") {
+//         document.getElementById("Eoutput").style.backgroundColor = "#32CD32"
+//     }  else { 
+      
+//     } 
+// });
+
+
+// document.querySelectorAll(".outputs").forEach(el => {
+//     el.style.backgroundColor = "#ff0000";
+//   });
+
+
