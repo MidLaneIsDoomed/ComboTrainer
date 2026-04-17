e4e3bcb189d5495f2aa4335e5b3a2de2704fac66
MidLaneIsDoomed <leo.ahlstrom08@gmail.com>
2026-04-18 01:14:00 +0200

    logic.js

--- JS/logic.js
+++ JS/logic.js
@@ -1,19 +1,76 @@
-const airblade = ["e", "q", "r" ]
+const eqFlash = ["e", "q"]
 let position = 0;
-
-const pressedKey = document.getElementById("pressedKey-container");
-
-const clickArea = document.getElementById("pressBorder");
-const clickBlocker = document.getElementById("clickBlocker")
 
 const actions = [
     () => document.getElementById("Eoutput").style.backgroundColor = "#32CD32",
-    () => document.getElementById("Qoutput").style.backgroundColor = "#32CD32",
-    () => document.getElementById("Routput").style.backgroundColor = "32CD32"
+    () => document.getElementById("Qoutput").style.backgroundColor = "#32CD32"
+]
+
+const Fkey = document.getElementById("Foutput");
+const clickAreaF = document.getElementById("second-dummy-F");
+
+
+let comboTimer = document.getElementById("combo-timer")
+timer = 1.00;
+
+const clickArea = document.getElementById("first-dummy");
+
+document.getElementById("first-dummy").onmouseover = function() {mouseOver()};
+document.getElementById("first-dummy").onmouseout = function() {mouseOut()};
+
+document.getElementById("second-dummy-F").onmouseover = function() {mouseOverF()};
+document.getElementById("second-dummy-F").onmouseout = function() {mouseOutF()};
+
+const output = document.querySelectorAll("#first-output div")
+
+
+clickArea.addEventListener("keydown", (event1) => {
+    if(event1.key === eqFlash[position]) {
+        actions[position]();
+        position++;
+
+        if(position === eqFlash.length) {
+
+            console.log("1completed");
+
+            clickAreaF.addEventListener("keydown", (eventF)=> {
+                if(eventF.key === "f") {
+                    Fkey.style.backgroundColor = "#32CD32"
+                    clickAreaF.style.display = "none"
+                    clickArea2.style.display = "block"
+                }
+            })
+
+        }
+
+    } else {
+         position = 0;
+        
+            output.forEach(el => {
+                el.style.backgroundColor = "#ff0000";
+            }); 
+
+    }
+    });
+
+
+const airblade = ["e", "q", "r" ]
+let position2 = 0;
+
+const clickArea2 = document.getElementById("second-dummy");
+
+const secondOuput = document.querySelectorAll("#second-output div")
+
+const actions2 = [
+    () => document.getElementById("2Eoutput").style.backgroundColor = "#32CD32",
+    () => document.getElementById("2Qoutput").style.backgroundColor = "#32CD32",
+    () => document.getElementById("2Routput").style.backgroundColor = "32CD32"
 ];
 
 let points = 0;
-    
+
+document.getElementById("second-dummy").onmouseover = function() {mouseOver2()};
+document.getElementById("second-dummy").onmouseout = function() {mouseOut2()};
 
 /* Reset your stats */
 function resetStats() {
@@ -22,34 +79,34 @@ function resetStats() {
 }
 
 /* The combo itself */
-clickArea.addEventListener("keydown", (event) => {
-    if (event.key === airblade[position]) {
+clickArea2.addEventListener("keydown", (event) => {
+    if (event.key === airblade[position2]) {
 
-        actions[position]();
-        position++;
+        actions2[position2]();
+        position2++;
         
 
-      if (position === airblade.length) {
+      if (position2 === airblade.length) {
         console.log("airblade completed!");
-        position = 0;
+        position2 = 0;
 
-        clickArea.style.display = "none";
+        clickArea2.removeAttribute("tabindex");
 
         document.getElementById("end-message").innerHTML = "Combo Completed"
         document.getElementById("restart").innerHTML = "Press SPACE to try again"
 
-        document.getElementById("points").innerHTML = ++points;
+        document.getElementById("points").innerHTML = "Score: " + ++points;
 
-        document.querySelectorAll(".outputs").forEach(el => { 
+        secondOuput.forEach(el => { 
             el.style.backgroundColor = "#32CD32";
         });
 
       }
   
     } else {
-      position = 0;
+      position2 = 0;
       
-        document.querySelectorAll(".outputs").forEach(el => {
+        secondOuput.forEach(el => {
             el.style.backgroundColor = "#ff0000";
         });
     }
@@ -59,11 +116,9 @@ clickArea.addEventListener("keydown", (event) => {
   document.addEventListener("keydown", (event2) => {
      if(event2.key === " ") {
 
-        clickArea.style.display = "block";
+        clickArea2.setAttribute("tabindex", "0");
 
-        clickArea.focus();
-
-        document.querySelectorAll(".outputs").forEach(el => {
+        secondOuput.forEach(el => {
             el.style.backgroundColor = "#ff0000";
 
         document.getElementById("end-message").innerHTML = ""
@@ -73,24 +128,29 @@ clickArea.addEventListener("keydown", (event) => {
     }
 });
 
-  
+/* Stops focus if you dont hover on second dummy */
+function mouseOut2() {
+    clickArea2.blur();
+};
+
+/* Starts focus if you hover over second dummy */
+function mouseOver2() {
+    clickArea2.focus();
+};
 
 
+function mouseOver() {
+    clickArea.focus();
+}
 
+function mouseOut() {
+    clickArea.blur();
+}
 
+function mouseOverF() {
+    clickAreaF.focus();
+}
 
-
-// keyPressed.addEventListener("keydown",function(event) {
-//     if(event.key === "e") {
-//         document.getElementById("Eoutput").style.backgroundColor = "#32CD32"
-//     }  else { 
-      
-//     } 
-// });
-
-
-// document.querySelectorAll(".outputs").forEach(el => {
-//     el.style.backgroundColor = "#ff0000";
-//   });
-
-
+function mouseOutF() {
+    clickAreaF.blur();
+}
